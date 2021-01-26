f1842c98bba4fa578f7a3a3f339b8cfdb3c90ef7
PMW <westpie@outlook.fr>
2021-01-26 17:10:00 +0100

    demo FC v1.0

--- bluetooth/config/routes.php
+++ bluetooth/config/routes.php
@@ -51,7 +51,7 @@ $routes->scope('/', function (RouteBuilder $builder) {
      * to use (in this case, templates/Pages/home.php)...
      */
     $builder->connect('/', ['controller' => 'Pages', 'action' => 'display', 'home']);
-    $builder->connect('/generate_dummy_data', ['controller' => 'productions', 'action' => 'generate', 'gen_dummy_data']);
+    $builder->connect('/generate_dummy_data', ['controller' => 'Productions', 'action' => 'generate', 'gen_dummy_data']);
 
     /*
      * ...and connect the rest of 'Pages' controller's URLs.
